9cdf6036ea554b88c9b8b73349f7980022109fd6
Lucas Torres <senomar_detritos@yahoo.com.br>
2024-09-02 13:53:38 -0300

    fix: menuside responsivel

--- app/src/components/MenuSide.vue
+++ app/src/components/MenuSide.vue
@@ -2,13 +2,22 @@
 	<div class="menu-side">
 		<ul>
 			<li>
-				<a href="#" @click="cadastrar">Adicionar Lista</a>
+				<a href="#" @click="cadastrar">
+					<i class="bi-file-earmark-plus"></i>
+					<span>Adicionar Lista</span>
+				</a>
 			</li>
 			<li>
-				<RouterLink to="/minhas-listas">Minhas Listas</RouterLink>
+				<RouterLink to="/minhas-listas">
+					<i class="bi-list-check"></i>
+					<span>Minhas Listas</span>
+				</RouterLink>
 			</li>
 			<li>
-				<a href="#" @click="sair">Sair</a>
+				<a href="#" @click="sair">
+					<i class="bi-box-arrow-left"></i>
+					<span>Sair</span>
+				</a>
 			</li>
 		</ul>
 	</div>
@@ -51,14 +60,40 @@
 		width: 300px;
 		height: calc(100vw - 52px);
 		border-right: 1px solid #eee;
+		display: none;
 	}
 
 	.menu-side li {
 		list-style: none;
 	}
 
-	.menu-side li a {
+	.menu-side li i,
+	.menu-side li span {
 		display: block;
-		padding: 8px 0px;
+		padding: 8px;
+		color: var(--bg-dark);
+	}
+
+	.menu-side li a {
+		display: flex;
+		align-items: center;
+		justify-content: flex-start;
+		color: var(--bg-dark);
+	}
+
+	@media (min-width: 800px) {
+		.menu-side {
+			display: block;
+		}
+	}
+
+	@media (max-width: 1200px) {
+		.menu-side {
+			padding: 12px;
+			width: 60px;
+		}
+		.menu-side li a span {
+			display: none;
+		}
 	}
 </style>
